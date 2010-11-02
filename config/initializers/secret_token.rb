fa6280cb6f51ea446dbba9c8c35b616d348ce83d
Mason Fischer <moocowmason@gmail.com>
2010-11-01 21:37:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ventana::Application.config.secret_token = '31149f2f06de05b7b7e8282e7d589dfcf2d4f3d84dc1b197674336eb2b11b63155a30c061049d227cf353c7789beb2c83eae568b15e6e22f04500d70ac8c0ddf'
